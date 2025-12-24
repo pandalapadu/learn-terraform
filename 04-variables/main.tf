0b97c6efa28c198f2b7c6d3c15c13f01189f1b70
VenkatReddy Kunam <venkat@gmail.com>
2025-12-24 22:21:06 +0530

    terraform

--- 04-variables/main.tf
+++ 04-variables/main.tf
@@ -35,7 +35,7 @@ output "output_2" {
   value = var.plain_variable
 }
 output "output_3" {
-  value = var.list_variable[1]
+  value = var.list_variable[0]
 }
 output "output_4" {
   value = var.map_variable
