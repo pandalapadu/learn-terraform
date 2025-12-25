42c5b895afce7f447e6f15d930ab6912dc01aaae
VenkatReddy Kunam <venkat@gmail.com>
2025-12-25 12:08:08 +0530

    terraform

---
+++ 07-loops-count/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "af504235-2f6d-4469-aa25-251f498730fc"
+}
+resource "null_resource" "demo" {
+  count = 10
+}
