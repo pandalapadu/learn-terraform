15da56ee81b05fffbd2a2e20b0e7f81985440114
VenkatReddy Kunam <venkat@gmail.com>
2025-12-25 13:18:07 +0530

    terraform

---
+++ 08-loops-forEach/main.tf
@@ -0,0 +1,13 @@
+resource "local_file" "foo" {
+  for_each = var.demo
+  content = each.value
+  filename = "/tmp/${each.key}"
+}
+
+variable "demo" {
+  default = [
+    ORANGE = "orange",
+    BANANA = "banana"
+
+  ]
+}
