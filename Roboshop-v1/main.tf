76571aff1fbe56250c30c63c544a61f57a3dbe00
VenkatReddy Kunam <venkat@gmail.com>
2025-12-25 14:03:03 +0530

    terraform

--- Roboshop-v1/main.tf
+++ Roboshop-v1/main.tf
@@ -1,8 +1,47 @@
-module "frontend" {
+module "components" {
+  for_each = var.components
   source = "./vm"
-  component = "frontend"
+  component = each.value["name"]
+  vm_size = each.value["vm_size"]
 }
-module "catalogue" {
-  source = "./vm"
-  component = "catalogue"
+
+variable "components" {
+  default = {
+    frontend = {
+      name        = "frontend"
+      vm_size     = "Standard_D2s_v3"
+    }
+    mongodb = {
+      name        = "mongodb"
+      vm_size     = "Standard_D2s_v3"
+    }
+    catalogue = {
+      name        = "catalogue"
+      vm_size     = "Standard_D2s_v3"
+    }
+    user = {
+      name        = "user"
+      vm_size     = "Standard_D2s_v3"
+    }
+    cart = {
+      name        = "cart"
+      vm_size     = "Standard_D2s_v3"
+    }
+    mysql = {
+      name        = "mysql"
+      vm_size     = "Standard_D2s_v3"
+    }
+    shipping = {
+      name        = "shipping"
+      vm_size     = "Standard_D2s_v3"
+    }
+    payment = {
+      name        = "payment"
+      vm_size     = "Standard_D2s_v3"
+    }
+    rabbitmq = {
+      name        = "rabbitmq"
+      vm_size     = "Standard_D2s_v3"
+    }
+  }
 }
