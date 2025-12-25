7d132e173c23307f8ae93e4184954de887dbff2a
VenkatReddy Kunam <venkat@gmail.com>
2025-12-25 11:45:04 +0530

    terraform

--- Roboshop-v1/vm/main.tf
+++ Roboshop-v1/vm/main.tf
@@ -38,7 +38,6 @@ resource "azurerm_virtual_machine" "main" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = var.component
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
