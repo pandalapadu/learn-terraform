95bc80322fabf8a831ec8bea192c7e0489585bd1
VenkatReddy Kunam <venkat@gmail.com>
2025-12-25 11:28:19 +0530

    terraform

--- Roboshop-v1/vm/main.tf
+++ Roboshop-v1/vm/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_public_ip" "main" {
   name = "${var.component}-pip"
   location = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
   sku = "Standard"
   ip_version = "IPv4"
 }
