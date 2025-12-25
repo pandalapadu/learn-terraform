f1c6088cde285827b77a27700353419b95debbbb
VenkatReddy Kunam <venkat@gmail.com>
2025-12-25 22:32:30 +0530

    terraform

--- Roboshop-v1/vm/main.tf
+++ Roboshop-v1/vm/main.tf
@@ -100,7 +100,6 @@ resource "azurerm_virtual_machine" "main" {
 resource "null_resource" "install_tools" {
   provisioner "remote-exec" {
     inline = [
-      "sudo dnf update -y",
       "sudo dnf install -y git",
       "sudo dnf install -y https://rpm.releases.hashicorp.com/RHEL/9/x86_64/stable/terraform-1.8.5-1.x86_64.rpm",
       "sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && sudo dnf install -y https://packages.microsoft.com/config/rhel/9.0/packages-microsoft-prod.rpm && sudo dnf install -y azure-cli",
