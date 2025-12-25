7c1c968e510337863fc8bd67d3c7378328c4aacb
VenkatReddy Kunam <venkat@gmail.com>
2025-12-25 15:05:07 +0530

    terraform

--- Roboshop-v1/vm/main.tf
+++ Roboshop-v1/vm/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_virtual_machine" "main" {
   # delete_data_disks_on_termination = true
 
   storage_image_reference {
-    community_gallery_image_id  = "/CommunityGalleries/LDOTrail-a8215d2e-c9a8-43ef-904d-c8b1ffb29cf7/Images/rhel9-devops-practice/Versions/latest"
+    source_image_id   = "/CommunityGalleries/LDOTrail-a8215d2e-c9a8-43ef-904d-c8b1ffb29cf7/Images/rhel9-devops-practice/Versions/latest"
   }
   storage_os_disk {
     name              = var.component
