4c71d2b18babf3a9f28d0bc085e03c255b7b0781
VenkatReddy Kunam <venkat@gmail.com>
2026-01-07 09:51:18 +0530

    learn terraform

--- 11-vault/main.tf
+++ 11-vault/main.tf
@@ -23,17 +23,19 @@ variable "token" {
 }
 
 # Read secret from Vault KV v2
-data "vault_kv_secret" "ssh_secret" {
+data "vault_kv_secret_v2" "ssh_secret" {
   mount = "demo-ssh"  # KV engine mount path
   name  = "venkat"     # Secret key name
 }
 
+# Output the password
 output "ssh_password" {
-  value     = data.vault_kv_secret.ssh_secret.data["password"]
+  value     = data.vault_kv_secret_v2.ssh_secret.data["password"]
   sensitive = true
 }
 
+# Save the password locally to a file (not recommended for production!)
 resource "local_file" "local" {
   filename = "/tmp/pass"
-  content = data.vault_kv_secret.ssh_secret
+  content  = data.vault_kv_secret_v2.ssh_secret.data["password"]
 }
