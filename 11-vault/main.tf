29af7c5f3d77975a962fc84218e2bebfaf5fd1d6
VenkatReddy Kunam <venkat@gmail.com>
2026-01-07 09:16:51 +0530

    learn terraform

---
+++ 11-vault/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "5.3.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = "http://vault.azdevopsvenkat.site:8200"
+  token   = var.token
+}
+
+variable "token" {
+  description = "Vault token to authenticate"
+  type        = string
+  sensitive   = true
+}
+
+data "vault_kv_secret_v2" "ssh_secret" {
+  path = "demo-ssh/venkat"
+}
+
+output "ssh_password" {
+  value     = data.vault_kv_secret_v2.ssh_secret.data["password"]
+  sensitive = true
+}
